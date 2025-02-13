856ae0645a79a686907e9499fbf551ce9339bdeb
danie <danie@192.168.0.150>
2025-02-12 19:27:18 -0700

    UPDATE

--- Ventana/src/module-info.java
+++ Ventana/src/module-info.java
@@ -5,4 +5,5 @@
  * 
  */
 module Ventana {
+	requires java.desktop;
 }
--- Ventana/src/ventana/Ventana.java
+++ Ventana/src/ventana/Ventana.java
@@ -1,19 +1,37 @@
-/**
- * 
- */
 package ventana;
 
-/**
- * 
- */
-public class Ventana {
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+ 
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+public class Ventana extends JFrame {
 
-	}
+    public Ventana() {
+    	
+    	
+    	setTitle("Ventana");
+        setLayout(null);
+        setSize(525, 500);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
 
+
+        JLabel label = new JLabel("click aqui");
+        label.setBounds(230, 100, 60, 60);
+        add(label);
+
+        JButton boton = new JButton("Boton");
+        boton.setBounds(200, 150, 100, 30);
+        add(boton);
+
+        
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new Ventana(); 
+    }
 }
+
