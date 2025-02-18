d15c3af6c38c34665aef6bf951543b5a3070ca02
danie <danie@192.168.0.150>
2025-02-17 20:36:00 -0700

    Diseño de login

--- Ventana/src/ventana/Ventana.java
+++ Ventana/src/ventana/Ventana.java
@@ -1,8 +1,8 @@
 package ventana;
 
+
 import java.awt.Color;
 import java.awt.Font;
-
 import javax.swing.*;
 
 public class Ventana extends JFrame {
@@ -10,38 +10,48 @@ public class Ventana extends JFrame {
 	public Ventana() {
     	
     	
-    	setTitle("Ventana");
-        setLayout(null);
-        setSize(525, 500);
-        setLocationRelativeTo(null);
-        setDefaultCloseOperation(EXIT_ON_CLOSE);
-
+        JFrame frame = new JFrame("Login");
+        frame.setSize(400, 250);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLayout(null);
+        frame.setLocationRelativeTo(null);
+        
         JPanel panel = new JPanel();
         panel.setLayout(null);
-        panel.setBackground(Color.white);
-        panel.setOpaque(true);
-        panel.setBounds(50,50,400,400);
-        add(panel);
-
+        panel.setBounds(0, 0, 400, 250);
+        frame.add(panel);
+        
+        JLabel titleLabel = new JLabel("Iniciar sesión");
+        titleLabel.setFont(new Font("Arial", Font.BOLD, 20));
+        titleLabel.setBounds(130, 10, 200, 30);
+        panel.add(titleLabel);
+        //Etiqueta correo
+        JLabel emailLabel = new JLabel("Correo:");
+        emailLabel.setBounds(50, 50, 100, 25);
+        panel.add(emailLabel);
+        //Field del correo
+        JTextField emailField = new JTextField();
+        emailField.setBounds(150, 50, 200, 25);
+        panel.add(emailField);
+        
+        // Etiqueta contraseña
+        JLabel passwordLabel = new JLabel("Contraseña:");
+        passwordLabel.setBounds(50, 90, 100, 25);
+        panel.add(passwordLabel);
+        //Password
+        JPasswordField passwordField = new JPasswordField();
+        passwordField.setBounds(150, 90, 200, 25);
+        panel.add(passwordField);
         
         
-        JLabel label = new JLabel("Bienvenido");
-        label.setBounds(160, 100, 80, 30);
-        label.setBackground(Color.CYAN);
-        label.setOpaque(true);
-        label.setFont(new Font("Italika",Font.BOLD, 12));
-        label.setHorizontalAlignment(SwingConstants.CENTER);
-        panel.add(label);
+        
+        JButton loginButton = new JButton("Iniciar sesión");
+        loginButton.setBounds(150, 160, 150, 30);
+        panel.add(loginButton);
         
         
-
-
-        JButton boton = new JButton("Boton");
-        boton.setBounds(150, 160, 100, 30);
-        panel.add(boton);
-
-        
-        setVisible(true);
+   
+        frame.setVisible(true);
     }
 
     public static void main(String[] args) {
