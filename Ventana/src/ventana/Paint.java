477d5ef1189a9f4ffa2b3caa12c3ef950154a33f
danie <danie@DESKTOP-VG8C0T4>
2025-04-01 02:34:11 -0700

    Interfaz de Paint en Java Swing

--- Ventana/src/ventana/Paint.java
+++ Ventana/src/ventana/Paint.java
@@ -1,5 +1,6 @@
 package ventana;
 
+import java.awt.BasicStroke;
 import java.awt.Color;
 import java.awt.EventQueue;
 
@@ -8,21 +9,40 @@ import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import java.awt.GridLayout;
 import java.awt.Image;
+import java.awt.Point;
 
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
+import java.util.ArrayList;
+import java.util.List;
 import java.awt.event.ActionEvent;
 import java.awt.FlowLayout;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import javax.swing.JSlider;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+
+
 import javax.swing.JComboBox;
 import javax.swing.BorderFactory;
 import javax.swing.ImageIcon;
 import javax.swing.JLabel;
 import javax.swing.SwingConstants;
 
-public class Paint extends JFrame {
+public class Paint extends JFrame implements MouseListener,MouseMotionListener{
 
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
+    private List<Point> puntos = new ArrayList<>();
+    private List<Trazo> trazos = new ArrayList<>();
+    private Color colorActual = Color.BLACK; 
+    private JPanel panel; 
+    private int grosor = 3;
+
 
 	/**
 	 * Launch the application.
@@ -52,16 +72,50 @@ public class Paint extends JFrame {
 		
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
+				
+		panel = new JPanel() {
+            @Override
+            protected void paintComponent(Graphics g) {
+                super.paintComponent(g);
+                Graphics2D g2 = (Graphics2D) g;
+                
+                for (Trazo trazo : trazos) {
+                    g2.setColor(trazo.color);
+                    g2.setStroke(new BasicStroke(trazo.grosor)); 
+                    for (int i = 1; i < trazo.puntos.size(); i++) {
+                        Point p1 = trazo.puntos.get(i - 1);
+                        Point p2 = trazo.puntos.get(i);
+                        g2.drawLine(p1.x, p1.y, p2.x, p2.y);
+                    }
+                }
+            }
+
+		};
 		
-		JPanel panel = new JPanel();
+        panel.addMouseListener(this);
+        panel.addMouseMotionListener(this); 
+	
 		panel.setBounds(257, 11, 717, 539);
 		panel.setBorder(BorderFactory.createLineBorder(Color.BLACK, 2));
 		contentPane.add(panel);
+		
+
 		
 		JPanel panel_1 = new JPanel();
 		panel_1.setBounds(10, 11, 237, 539);
 		contentPane.add(panel_1);
 		panel_1.setLayout(null);
+		
+		JSlider sliderGrosor = new JSlider(JSlider.HORIZONTAL, 1, 20, 3);
+		sliderGrosor.setBounds(47, 73, 150, 20);
+		panel_1.add(sliderGrosor);
+		sliderGrosor.addChangeListener(new ChangeListener() {
+		    @Override
+		    public void stateChanged(ChangeEvent e) {
+		        grosor = sliderGrosor.getValue();  
+		        panel.repaint();
+		    }
+		});
 		
 		JPanel panel_2 = new JPanel();
 		panel_2.setBounds(10, 300, 217, 228);
@@ -69,40 +123,48 @@ public class Paint extends JFrame {
 		panel_2.setLayout(new GridLayout(3, 3, 10, 10));
 		
 		JButton btnNewButton_1 = new JButton("");
-		
+		btnNewButton_1.addActionListener(e -> colorActual = new Color(255,0,0));
 		btnNewButton_1.setForeground(new Color(255, 0, 0));
 		btnNewButton_1.setBackground(new Color(255, 0, 0));
 		panel_2.add(btnNewButton_1);
 		
 		JButton btnNewButton = new JButton("");
+		btnNewButton.addActionListener(e -> colorActual = new Color(255, 255, 128));
 		btnNewButton.setBackground(new Color(255, 255, 128));
 		panel_2.add(btnNewButton);
 		
 		JButton btnNewButton_2 = new JButton("");
+		btnNewButton_2.addActionListener(e -> colorActual = new Color(255, 128, 255));
 		btnNewButton_2.setBackground(new Color(255, 128, 255));
 		panel_2.add(btnNewButton_2);
 		
 		JButton btnNewButton_3 = new JButton("");
+		btnNewButton_3.addActionListener(e -> colorActual = new Color(0, 64, 0));
 		btnNewButton_3.setBackground(new Color(0, 64, 0));
 		panel_2.add(btnNewButton_3);
 		
 		JButton btnNewButton_4 = new JButton("");
+		btnNewButton_4.addActionListener(e -> colorActual = new Color(64, 0, 128));
 		btnNewButton_4.setBackground(new Color(64, 0, 128));
 		panel_2.add(btnNewButton_4);
 		
 		JButton btnNewButton_5 = new JButton("");
+		btnNewButton_5.addActionListener(e -> colorActual = new Color(0, 64, 128));
 		btnNewButton_5.setBackground(new Color(0, 64, 128));
 		panel_2.add(btnNewButton_5);
 		
 		JButton btnNewButton_6 = new JButton("");
+		btnNewButton_6.addActionListener(e -> colorActual = new Color(128, 255, 128));
 		btnNewButton_6.setBackground(new Color(128, 255, 128));
 		panel_2.add(btnNewButton_6);
 		
 		JButton btnNewButton_7 = new JButton("");
+		btnNewButton_7.addActionListener(e -> colorActual = new Color(128, 128, 0));
 		btnNewButton_7.setBackground(new Color(128, 128, 0));
 		panel_2.add(btnNewButton_7);
 		
 		JButton btnNewButton_8 = new JButton("");
+		btnNewButton_8.addActionListener(e -> colorActual = new Color(255, 0, 128));
 		btnNewButton_8.setBackground(new Color(255, 0, 128));
 		panel_2.add(btnNewButton_8);
 		
@@ -158,5 +220,64 @@ public class Paint extends JFrame {
 		lblNewLabel.setBackground(new Color(255, 255, 255));
 		lblNewLabel.setBounds(10, 11, 217, 34);
 		panel_1.add(lblNewLabel);
+
+	}
+	
+	class Trazo {
+	    List<Point> puntos;
+	    int grosor;
+	    Color color;
+	    public Trazo(int grosor,Color color) {
+	        this.puntos = new ArrayList<>();
+	        this.grosor = grosor;
+	        this.color = color;
+	    }
+	}
+
+	
+
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mousePressed(MouseEvent e) {
+	    Trazo nuevoTrazo = new Trazo(grosor,colorActual); 
+	    nuevoTrazo.puntos.add(e.getPoint());
+	    trazos.add(nuevoTrazo);
+	    panel.repaint();
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseExited(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseDragged(MouseEvent e) {
+	    if (!trazos.isEmpty()) {
+	        trazos.get(trazos.size() - 1).puntos.add(e.getPoint()); 
+	        panel.repaint();
+	    }
+	}
+	@Override
+	public void mouseMoved(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
 	}
 }
